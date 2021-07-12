87588934cf16223de923d29269517a885025b6b7
magacode <48868211+magacode@users.noreply.github.com>
2021-07-12 08:27:48 +0000

    Initial commit

---
+++ tests/capitalize.test.js
@@ -0,0 +1,9 @@
+// нажмите кнопку run чтобы запустить тесты
+// попробуйте изменять код функции / тестов, запуская проверки заново
+import { strict as assert } from 'assert';
+import { capitalize } from '../src/capitalize.js';
+
+assert.equal(capitalize('hello'), 'Hello');
+assert.equal(capitalize(''), '');
+
+console.log('Все тесты пройдены!');
